019ea14cc6e80e9acda15557e7bd70a08a0ec9f9
Bhagwat Chate <bhagwat.chate25@gmail.com>
2025-08-07 07:28:07 +0530

    doc ingestion- read pdf

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -8,19 +8,58 @@ from exception.custom_exception import DocumentPortalException
 from dotenv import load_dotenv
 
 load_dotenv()
+log = CustomLogger().get_logger(__name__)
 
 
-class DocumentComparator:
+class DocumentIngestion:
 
-    def __init__(self):
-        pass
+    def __init__(self, base_dir):
+        try:
+            self.base_dir = Path(base_dir)
+            self.base_dir.mkdir(parents=True, exist_ok=True)
+
+        except Exception as e:
+            log.error(f"error in initializing DocumentIngestion: {str(e)}")
+            raise DocumentPortalException(f"error in initializing DocumentIngestion: {str(e)}", sys)
 
     def delete_existing_files(self, file_paths):
-        pass
+        try:
+
+            pass
+
+        except Exception as e:
+            log.error(f"error in delete_existing_files: {str(e)}")
+            raise DocumentPortalException(f"error in delete_existing_files: {str(e)}", sys)
 
     def save_pdf(self):
-        pass
+        try:
 
-    def read_pdf(self):
-        pass
+            pass
 
+        except Exception as e:
+            log.error(f"error in save_pdf: {str(e)}")
+            raise DocumentPortalException(f"error in save_pdf: {str(e)}", sys)
+
+    def read_pdf(self, pdf_path: Path) -> str:
+        try:
+
+            with fitz.open(self, pdf_path) as doc:
+                if doc.is_encrypted():
+                    raise ValueError("PDF is encrypted and can not be read")
+
+                all_text = []
+
+                for page_num in range(len(doc.page_count)):
+                    page = doc.load_page(page_num)
+                    text = page.get_text()
+
+                    if text.strip():
+                        all_text.append(f"\n --- Page {page_num + 1} --- \n {text}")
+
+            log.info("PDF read successful", file=str(pdf_path), pages=len(all_text))
+
+            return "\n".join(all_text)
+
+        except Exception as e:
+            log.error(f"error in read_pdf: {str(e)}")
+            raise DocumentPortalException(f"error in read_pdf: {str(e)}", sys)
--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -6,18 +6,28 @@ from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import ChangeFormat, SummaryResponse
 from prompt.prompt_library import PROMPT_REGISTRY
-from utils.model_loader import load_config
+from utils.model_loader import ModelLoader
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
 
 load_dotenv()
-
 log = CustomLogger().get_logger(__name__)
 
 
 class DocumentCompareLLM:
     def __init__(self):
-        pass
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_model()
+            self.parser = JsonOutputParser()
+            self.fixing_parser = OutputFixingParser(parser=self.parser, llm=self.llm)
+            self.prompt = PROMPT_REGISTRY['document_comparison_prompt']
+            self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
+
+            log.info("DocumentCompareLLM initialized with chain, parser and model")
+        except Exception as e:
+            log.erroe(f"error in initializing DocumentCompareLLM: {str(e)}")
+            raise DocumentPortalException(f"error in initializing DocumentCompareLLM: {str(e)}", sys)
 
     def compare_documents(self):
         pass
