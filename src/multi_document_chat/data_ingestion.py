d84474f0786463c29a1f200c2f3d22c7c9d1f6b6
Bhagwat Chate <bhagwat.chate25@gmail.com>
2025-08-09 14:43:02 +0530

    data, core skeleton for multi doc chat

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,13 @@
+from logger.custom_logger import CustomLogger
+
+
+class DocumentIngestor:
+    def __init__(self):
+        pass
+
+    def ingest_files(self):
+        pass
+
+    def _create_retriever(self, document):
+        pass
+
