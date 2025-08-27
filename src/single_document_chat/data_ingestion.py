e1e60be5ee5b473d0d5323b87d80c9ea2008b39d
Bhagwat Chate <bhagwat.chate25@gmail.com>
2025-08-27 08:49:54 +0530

    data file ingestion in single doc chat

--- src/single_document_chat/data_ingestion.py
+++ src/single_document_chat/data_ingestion.py
@@ -1,10 +1,13 @@
 import sys
 import uuid
 from pathlib import Path
+from typing import List
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from utils.model_loader import ModelLoader
+from datetime import datetime
+from datetime import timezone
 
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
@@ -14,21 +17,49 @@ log = CustomLogger.get_logger(__name__)
 
 class SingleDocIngestor:
 
-    def __init__(self):
+    def __init__(self, data_dir: str = 'data/single_document_chat', faiss_dir: str = 'faiss_index'):
         try:
-            pass
+            self.data_dir = Path(data_dir)
+            self.data_dir.mkdir(parents=True, exist_ok=True)
+            self.file_path = self.data_dir / f"{uuid.uuid4()}.pdf"
+
+            self.faiss_dir = Path(faiss_dir)
+            self.faiss_dir.mkdir(parents=True, exist_ok=True)
+
+            self.model_loader = ModelLoader()
+
+            log.info("class SingleDocIngestor initialized with", temp_path=str(self.data_dir), faiss_path=str(self.faiss_dir))
+
         except Exception as e:
             log.error('failed to initialize class SingleDocIngestor', error=str(e))
             raise DocumentPortalException('initialization error in class SingleDocIngestor', sys)
 
-    def ingest_files(self):
+    def ingest_files(self, uploaded_files):
         try:
-            pass
+            documents = []
+
+            for uploaded_file in uploaded_files:
+                unique_filename = f"session_{datetime.now(timezone.utc).strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}.pdf"
+                temp_path = self.data_dir / unique_filename
+
+                with open(temp_path, "wb") as f_out:
+                    f_out.write(uploaded_file.read())
+
+                log.info("PDF saved for ingestion", filename=uploaded_file.name)
+
+                loader = PyPDFLoader(str(temp_path))
+                docs = loader.load()
+                documents.extend(docs)
+
+            log.info("PDF files loaded", count=len(documents))
+
+            return self._create_retriever(documents)
+
         except Exception as e:
             log.error('failed to initialize class SingleDocIngestor.ingest_files()', error=str(e))
             raise DocumentPortalException('initialization error in class SingleDocIngestor.ingest_files()', sys)
 
-    def create_retriever(self):
+    def _create_retriever(self, documents: List):
         try:
             pass
         except Exception as e:
