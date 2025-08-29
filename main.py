dfddd6347a80607938d84d2422740b6ec67ad60a
Bhagwat Chate <bhagwat.chate25@gmail.com>
2025-08-29 12:43:53 +0530

    ui added

--- main.py
+++
@@ -1,6 +0,0 @@
-from src.document_analyzer.data_ingestion import DocumentHandler
-
-
-if __name__ == '__main__':
-    obj = DocumentHandler()
-    obj.save_pdf("E:\\LLMOps\\document_portal\\data\\document_analysis\\NIPS-2017-attention-is-all-you-need-Paper.pdf")
